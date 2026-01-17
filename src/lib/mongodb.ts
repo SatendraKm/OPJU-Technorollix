2b44fda775fa173c26f270734fdfcd6a310150d1
Prince Raj <63579641+princeraj2572@users.noreply.github.com>
2026-01-17 23:47:59 +0530

    console log removed for connection strig

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -4,11 +4,6 @@ const NEXT_PUBLIC_MONGODB_URI: string =
 	process.env.NEXT_PUBLIC_MONGODB_URI || "";
 const NEXT_PUBLIC_MONGODB_DBNAME: string =
 	process.env.NEXT_PUBLIC_MONGODB_DBNAME || "";
-console.log(
-	"NEXT_PUBLIC_MONGODB_URI, NEXT_PUBLIC_MONGODB_DBNAME : ",
-	NEXT_PUBLIC_MONGODB_URI,
-	NEXT_PUBLIC_MONGODB_DBNAME
-);
 if (!NEXT_PUBLIC_MONGODB_URI) {
 	throw new Error(
 		"Please define the NEXT_PUBLIC_MONGODB_URI environment variable"
